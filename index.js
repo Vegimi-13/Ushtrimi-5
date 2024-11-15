0a043f35f6b53c7f2fb1d62a0801fa66f071f69f
Vegim Ahmetaj <vegimahmetaj@gmail.com>
2024-11-15 11:59:38 +0100

    detyra 5

--- index.js
+++ index.js
@@ -50,6 +50,28 @@ console.log( "Emri me i gjate eshte: " + emriMeIgjate);
 //Detyra 5
 
 
+let nota1 = 1;
+let nota2 = 2;
+let nota3 =3;
+let nota4 = 4;
+
+let performanca = "";
+
+let average = (nota1+nota2+nota3+nota4) /4;
+
+
+if(average >= 4.5){
+    performanca = "Shkelqyeshem";
+}else if(average>=3.5){
+    performanca = "i mesem";
+}else{
+    performanca = "I ulet";
+}
+
+console.log("Perfomanca eshte: " + performanca)
+
+
+
 
 
 
