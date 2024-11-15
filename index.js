606a4afc039b586536cc89b5c1842f27353d2cbe
Vegim Ahmetaj <vegimahmetaj@gmail.com>
2024-11-15 11:39:50 +0100

    Detyra 1

---
+++ index.js
@@ -0,0 +1,13 @@
+let emri1 = "Vegim";
+let emri2 = "Arber";
+let emri3 = "Erion";
+let emri4 = "James";
+let emri5 = "Bond";
+
+
+
+let emriconcat = emri1.concat(emri2,emri3,emri4,emri5);
+console.log(emriconcat);
+
+
+
