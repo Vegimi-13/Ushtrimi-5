406ff94202cde6634e02ea81b1b13906f1cc4ac3
Vegim Ahmetaj <vegimahmetaj@gmail.com>
2024-11-15 11:42:58 +0100

    Detyra 2

--- index.js
+++ index.js
@@ -11,3 +11,23 @@ console.log(emriconcat);
 
 
 
+// Detyra 2
+
+
+let numri1 = 1;
+let numri2 = 2;
+let numri3 = 3;
+let numri4 = 4;
+let numri5 = 5;
+
+
+let mesatarja = (numri1+numri2+numri3+numri4+numri5)/5;
+console.log("Mesatarja eshte " + mesatarja)
+
+
+
+
+
+
+
+
